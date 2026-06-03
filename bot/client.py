6411b6d9787c9e7a7510d2945ee26a74721fe155
Chaiti Bain <chaitibain1@gmail.com>
2026-06-03 15:04:22 +0530

    client.py

--- bot/client.py
+++ bot/client.py
@@ -1,1 +1,12 @@
+from binance.client import Client
 
+class BinanceClient:
+    def __init__(self, api_key, api_secret):
+        self.client = Client(
+            api_key,
+            api_secret,
+            testnet=True
+        )
+
+    def get_client(self):
+        return self.client
