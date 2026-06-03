fcf36add8bcfe39f31ac2d351edce1ae65d8d5b3
Chaiti Bain <chaitibain1@gmail.com>
2026-06-03 15:06:36 +0530

    orders.py

--- bot/orders.py
+++ bot/orders.py
@@ -1,1 +1,47 @@
+import logging
 
+class OrderManager:
+
+    def __init__(self, client):
+        self.client = client
+
+    def place_market_order(
+        self,
+        symbol,
+        side,
+        quantity
+    ):
+        response = self.client.futures_create_order(
+            symbol=symbol,
+            side=side,
+            type="MARKET",
+            quantity=quantity
+        )
+
+        logging.info(
+            f"MARKET ORDER: {response}"
+        )
+
+        return response
+
+    def place_limit_order(
+        self,
+        symbol,
+        side,
+        quantity,
+        price
+    ):
+        response = self.client.futures_create_order(
+            symbol=symbol,
+            side=side,
+            type="LIMIT",
+            quantity=quantity,
+            price=price,
+            timeInForce="GTC"
+        )
+
+        logging.info(
+            f"LIMIT ORDER: {response}"
+        )
+
+        return response
